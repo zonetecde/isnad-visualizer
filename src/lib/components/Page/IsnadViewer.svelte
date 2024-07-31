aab5cd5ea275e58da06322a1bb90db85ae4f3473
zonetecde <zonetecde@gmail.com>
2024-07-31 23:56:41 +0200

    small improvement

--- src/lib/components/Page/IsnadViewer.svelte
+++ src/lib/components/Page/IsnadViewer.svelte
@@ -2,11 +2,13 @@
 	import { selectedHadiths } from '$lib/stores/globalStore';
 	import { onMount } from 'svelte';
 
-	let imgPath: string = '';
+	let imgPath: string;
 
 	onMount(async () => {
 		// Génère le graphique
-		const reponse = await fetch('/api/flowchart-gen');
+		const reponse = await fetch('/api/flowchart-gen', {
+			method: 'POST'
+		});
 
 		if (reponse.ok) {
 			// Image du graphique
@@ -17,4 +19,6 @@
 	});
 </script>
 
-<img src={imgPath} alt="Graphique de la chaîne de transmission" />
+{#if imgPath}
+	<img src={imgPath} alt="Graphique de la chaîne de transmission" />
+{/if}
--- src/routes/api/flowchart-gen/+server.ts
+++ src/routes/api/flowchart-gen/+server.ts
@@ -2,7 +2,7 @@ import graphviz from 'graphviz';
 import fs from 'fs';
 
 /** @type {import('./types').RequestHandler} */
-export async function GET({ url }: { url: URL }) {
+export async function POST({ request }: { request: Request }) {
 	const graphNode = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
 	const graphEdge = [
@@ -19,20 +19,32 @@ export async function GET({ url }: { url: URL }) {
 
 	var g = graphviz.digraph('G');
 	g.set('dpi', 300);
+	//g.set('rankdir', 'LR');
+	g.set('rankdir', 'TB');
+	g.set('splines', 'ortho');
 
 	graphNode.forEach((node) => {
 		g.addNode(node, { shape: 'box', color: 'blue' });
 	});
 
 	graphEdge.forEach((edge) => {
-		g.addEdge(edge[0], edge[1]);
+		g.addEdge(edge[0], edge[1], { arrowhead: 'none' });
 	});
 
+	const randomName = Math.random().toString(36).substring(7);
+
 	g.setGraphVizPath('C:/Program Files/Graphviz/bin');
-	g.output('png', './static/flowchart/test01.png');
+	g.output('png', './static/flowchart/' + randomName + '.png');
+
+	// Tant que le fichier n'est pas créé, on attend
+	while (!fs.existsSync('./static/flowchart/' + randomName + '.png')) {
+		await new Promise((resolve) => setTimeout(resolve, 100));
+	}
+
+	await new Promise((resolve) => setTimeout(resolve, 100));
 
 	// return the image path
-	return new Response('/flowchart/test01.png', {
+	return new Response('/flowchart/' + randomName + '.png', {
 		headers: {
 			'content-type': 'text/plain'
 		}
